977cae837bb4bc1508ed0899d2659e9b25a8fc5b
wastefello21-hub <wastefello21@gmail.com>
2026-06-10 21:16:04 +0530

    new update

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,6 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  turbopack: {
-    root: 'C:\\Users\\mythr\\.gemini\\antigravity\\scratch\\ganesha-festival',
-  },
 };
 
 export default nextConfig;
